4489f6f9ef678436b7d5d426527bc463cc72fd82
janglad <jan.gladines@gmail.com>
2024-11-11 23:20:19 +0100

    add docs redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ import { createMDX } from "fumadocs-mdx/next";
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/docs",
+      permanent: false,
+    },
+  ],
+};
 
 export default withMDX(nextConfig);
